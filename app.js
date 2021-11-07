97bc129569ef79e83d716d41babaf154c6ab92d6
zahidkhan262 <zahidkhan007860@gmail.com>
2021-11-07 19:31:28 +0530

    add button by javascript

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-let todoList = document.getElementsByClassName('list');
+let todoList = document.getElementById('list');
+let todoInp = document.getElementById('todo-input');
+let del = document.getElementById('del');
+
+var inpValue = '';
+todoInp.addEventListener('input', function (e) {
+    console.log(e.target.value);
+    inpValue = e.target.value;
+})
 
 
 
-
-// var addTodo = document.getElementsByClassName('addBtn');
 
 
 function addTodo() {
-    var newList = document.createElement('li');
 
-    var newNode = document.createTextNode('khan' + (todoList.childElementCount + 1))
-    newList.appendChild(newNode);
-    todoList.appendChild(newList)
+    if (inpValue !== undefined && inpValue !== null && inpValue !== '') {
+
+        var newList = document.createElement('li');
+        var newNode = document.createTextNode(inpValue);
+        newList.appendChild(newNode);
 
 
-    console.log(newList)
+        var pan = document.createElement('span');
+        pan.className = 'addDel';
+
+        var fontAwsm = document.createElement('i');
+
+        fontAwsm.className = 'fa fa-trash';
+
+        pan.appendChild(fontAwsm);
+
+        newList.className = 'd-flex-1';
+        newList.appendChild(pan);
+
+        todoList.appendChild(newList);
+
+
+        console.log(newList);
+        // re initialize
+        todoInp.value = '';
+        inpValue = '';
+    } else {
+        alert('enter valid name')
+    }
+
 }
+
+// delete
+del.addEventListener('click', function () {
+    alert('del')
+})
+
